4ebb42053dae57fbdbd4256616daaecdd287679c
Yannick Mauray <yannick.mauray@gmail.com>
2026-04-21 23:50:52 +0200

    Update SHA256 checksum for macOS ARM64
    Updated the SHA256 checksum for the macOS ARM64 version of Johannes.

--- Formula/johannes.rb
+++ Formula/johannes.rb
@@ -7,7 +7,7 @@ class Johannes < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ymauray/johannes/releases/download/v#{version}/johannes-osx-arm64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "f6255e68d87ccc17d092322d32b81426f1117160a6ca1997cc45570cb4d07831"
     end
   end
 
