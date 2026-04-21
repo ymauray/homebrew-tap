260572cc3ca55981a15ca502fbc9bec2af751c80
Yannick Mauray <yannick.mauray@gmail.com>
2026-04-21 23:46:22 +0200

    Update download URLs for johannes package

--- Formula/johannes.rb
+++ Formula/johannes.rb
@@ -6,14 +6,14 @@ class Johannes < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ymauray/johannes/releases/download/v#{version}/excelst-osx-arm64.tar.gz"
+      url "https://github.com/ymauray/johannes/releases/download/v#{version}/johannes-osx-arm64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ymauray/johannes/releases/download/v#{version}/excelst-linux-x64.tar.gz"
+      url "https://github.com/ymauray/johannes/releases/download/v#{version}/johannes-linux-x64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
